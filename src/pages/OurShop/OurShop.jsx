69db13009e81f292b5b1d70c7f95c94661bd78ce
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-31 20:14:17 +0600

    Add tap and complite our shop page with responsive

--- src/pages/OurShop/OurShop.jsx
+++ src/pages/OurShop/OurShop.jsx
@@ -1,9 +1,11 @@
 import Cover from "../Share/Cover/Cover";
 import shopImg from "../../assets/shop/banner2.jpg"
+import ShopTab from "./ShopTab";
 const OurShop = () => {
     return (
         <div>
             <Cover img={shopImg} title = "Our Shop"  ></Cover>
+            <ShopTab></ShopTab>
         </div>
     );
 };
---
+++ src/pages/OurShop/ShopTab.jsx
@@ -0,0 +1,44 @@
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../components/useMenu/useMenu";
+import FoodCards from "../../components/FoodCards/FoodCards"
+
+const ShopTab = () => {
+    const [menu] = useMenu();
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const soups = menu.filter(item => item.category === 'soup');
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const drinks = menu.filter(item => item.category === 'drinks');
+  return (
+    <div className="my-12">
+      <Tabs>
+        <TabList className= "py-5 text-center lg:mx-32">
+          <Tab>SALAd</Tab>
+          <Tab>PIZZA</Tab>
+          <Tab>SOUPS</Tab>
+          <Tab>DESSERTS</Tab>
+          <Tab>DRINKS</Tab>
+        </TabList>
+
+        <TabPanel>
+          <FoodCards cards={salad} ></FoodCards>
+        </TabPanel>
+        <TabPanel>
+          <FoodCards cards={pizza} ></FoodCards>
+        </TabPanel> 
+        <TabPanel>
+          <FoodCards cards={soups} ></FoodCards>
+        </TabPanel>
+        <TabPanel>
+          <FoodCards cards={desserts} ></FoodCards>
+        </TabPanel>
+        <TabPanel>
+          <FoodCards cards={drinks} ></FoodCards>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default ShopTab;

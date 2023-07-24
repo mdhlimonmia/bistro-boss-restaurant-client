fe6aed6ec136feb1a853c66dcdf9e3de39e7f59f
mdhlimonmia <mdhlimonmia@gmail.com>
2023-07-24 12:55:45 -0700

    Show Selelt cart in my cart route

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,48 +1,112 @@
 import { Link, Outlet } from "react-router-dom";
-import { AiOutlineHome, AiOutlineBars, AiOutlineMenu, AiOutlineShopping, AiOutlineMail} from "react-icons/ai";
+import {
+  AiOutlineHome,
+  AiOutlineBars,
+  AiOutlineMenu,
+  AiOutlineShopping,
+  AiOutlineMail,
+} from "react-icons/ai";
 import { MdRestaurant } from "react-icons/md";
 import { GiWallet } from "react-icons/gi";
 import { BsStars } from "react-icons/bs";
 import { TbCalendarTime } from "react-icons/tb";
-import { FaUsers,FaBook, FaCalendarAlt, FaShoppingCart} from "react-icons/fa";
-
+import { FaUsers, FaBook, FaCalendarAlt, FaShoppingCart } from "react-icons/fa";
 
 const Dashboard = () => {
   return (
-    <div className="drawer drawer-open">
+    <div className="drawer drawer-mobile ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <div className="drawer-content bg-white">
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
         <Outlet></Outlet>
       </div>
-      <div className="drawer-side">
-        <div className="menu pt-10 pl-4 w-[250px] h-full bg-[#D1A054] text-[#151515] ">
-           <div className="pl-4">
-           <h1 className="font-[900] text-[24px] uppercase">BISTRO BOSS</h1>
-            <h2 className="font-[700] tracking-[5px] text-[17px] uppercase">
-              Restaurant
-            </h2>
-           </div>
-          <ul className="mt-14 uppercase pl-0 ml-0 font-[500] text-[16px]">
-            {/* Sidebar content here */}
-            <li><Link to ="/dashboard/adminhome"><AiOutlineHome></AiOutlineHome> ADMIN HOME</Link></li>
-            <li><Link to ="/dashboard/additems"><MdRestaurant></MdRestaurant>ADD ITEMS</Link></li>
-            <li><Link to ="/dashboard/manageitems"><AiOutlineBars></AiOutlineBars>MANAGE ITEMS</Link></li>
-            <li><Link to ="/dashboard/managebooking"><FaBook></FaBook>MANAGE BOOKING</Link></li>
-            <li><Link to ="/dashboard/allusers"><FaUsers></FaUsers>ALL USERS</Link></li>
+      <div className="drawer-side bg-[#D1A054] text-black">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80">
+          {/* Sidebar content here */}
+          <li>
+            <Link to="/dashboard/adminhome">
+              <AiOutlineHome></AiOutlineHome> ADMIN HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard/additems">
+              <MdRestaurant></MdRestaurant>ADD ITEMS
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard/manageitems">
+              <AiOutlineBars></AiOutlineBars>MANAGE ITEMS
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard/managebooking">
+              <FaBook></FaBook>MANAGE BOOKING
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard/allusers">
+              <FaUsers></FaUsers>ALL USERS
+            </Link>
+          </li>
 
-            <li><Link to ="/dashboard/userhome"><AiOutlineHome></AiOutlineHome> USER HOME</Link></li>
-            <li><Link to="reservation"><FaCalendarAlt></FaCalendarAlt> RESERVATION</Link></li>
-            <li><Link to="paymenthistory"><GiWallet></GiWallet>PAYMENT HISTORY</Link></li>
-            <li><Link to="mycart"><FaShoppingCart></FaShoppingCart>MY CART</Link></li>
-            <li><Link to="addreview"><BsStars></BsStars>ADD REVIEW</Link></li>
-            <li><Link to="mybooking"><TbCalendarTime></TbCalendarTime>MY BOOKING</Link></li>
-            <hr className="mr-8 ml-2" />
-             <li><Link to="/"><AiOutlineHome></AiOutlineHome>HOME</Link></li>
-             <li><Link to="/menu"><AiOutlineMenu></AiOutlineMenu>MENU</Link></li>
-             <li><Link to="/shop"><AiOutlineShopping></AiOutlineShopping>SHOP</Link></li>
-             <li><Link to="/contact"><AiOutlineMail></AiOutlineMail>CONTACT</Link></li>
-          </ul>
-        </div>
+          <li>
+            <Link to="/dashboard/userhome">
+              <AiOutlineHome></AiOutlineHome> USER HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="reservation">
+              <FaCalendarAlt></FaCalendarAlt> RESERVATION
+            </Link>
+          </li>
+          <li>
+            <Link to="paymenthistory">
+              <GiWallet></GiWallet>PAYMENT HISTORY
+            </Link>
+          </li>
+          <li>
+            <Link to="mycart">
+              <FaShoppingCart></FaShoppingCart>MY CART
+            </Link>
+          </li>
+          <li>
+            <Link to="addreview">
+              <BsStars></BsStars>ADD REVIEW
+            </Link>
+          </li>
+          <li>
+            <Link to="mybooking">
+              <TbCalendarTime></TbCalendarTime>MY BOOKING
+            </Link>
+          </li>
+          <hr className="mr-8 ml-2" />
+          <li>
+            <Link to="/">
+              <AiOutlineHome></AiOutlineHome>HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu">
+              <AiOutlineMenu></AiOutlineMenu>MENU
+            </Link>
+          </li>
+          <li>
+            <Link to="/shop">
+              <AiOutlineShopping></AiOutlineShopping>SHOP
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              <AiOutlineMail></AiOutlineMail>CONTACT
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,10 +1,103 @@
-
+import Swal from "sweetalert2";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useCart from "../../../components/useCart/useCart";
+import { RiDeleteBin5Line } from "react-icons/ri";
 const MyCart = () => {
-    return (
-        <div>
-            <h1>MyCart</h1>
+  const [cart, refetch] = useCart();
+
+  const handelDelete = (_id)=>{
+    console.log(_id);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`http://localhost:5000//carts/${_id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log("Limon");
+                    if (data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+        }
+    })
+
+  }
+  return (
+    <div>
+      <SectionTitle heading="WANNA ADD MORE?" subtitle="My Cart"></SectionTitle>
+
+      <div className="flex justify-center text-left">
+        <div className="">
+          <table className="md:w-[600px]">
+            {/* head */}
+              <tr className="bg-[#D1A054]  text-white h-[40px] rounded-xl my-10">
+                <th>
+                  <label>
+                    <p>#</p>
+                  </label>
+                </th>
+                <th>ITEM IMAGE</th>
+                <th>ITEM NAME</th>
+                <th>PRICE</th>
+                <th>ACTION</th>
+              </tr>
+
+              {cart.map((c, index) => (
+                <>
+                <tr key={c._id} className="my-10">
+                  <th>
+                    <label>
+                      <p>{index + 1}</p>
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={c.image}
+                            alt="food image"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                  <div>
+                        <div className="font-bold">{c.name}</div>
+                      </div>
+                  </td>
+                  <td>
+                    <span className="badge badge-ghost badge-sm">
+                      {c.price}
+                    </span>
+                  </td>
+                  <th>
+                    <button onClick={()=>handelDelete(c._id)} className="btn btn-ghost text-2xltext-white"><RiDeleteBin5Line></RiDeleteBin5Line></button>
+                  </th>
+                </tr>
+                  {/* <hr className="text-2xl w-10 text-red-700" /> */}
+                </>
+              ))}
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyCart;

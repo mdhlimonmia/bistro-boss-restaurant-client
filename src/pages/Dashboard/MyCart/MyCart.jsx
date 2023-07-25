03353f7fed7a165b21990db8aa2d40fcfd553ca5
mdhlimonmia <mdhlimonmia@gmail.com>
2023-07-25 20:26:50 +0600

    Reservation From Done

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -5,34 +5,37 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 const MyCart = () => {
   const [cart, refetch] = useCart();
 
-  const handelDelete = (_id)=>{
+  const totalPrice = cart.reduce((acc, c) => acc + c.price, 0);
+  // console.log("totla=", totalPrice);
+
+  const handelDelete = (_id) => {
     console.log(_id);
     Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
-        if (result.isConfirmed) {
-            fetch(`http://localhost:5000//carts/${_id}`, {
-                method: 'DELETE'
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log("Limon");
-                    if (data.deletedCount > 0) {
-                        refetch();
-                        Swal.fire(
-                            'Deleted!',
-                            'Your file has been deleted.',
-                            'success'
-                        )
-                    }
-                })
-        }
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${_id}`, {
+          method: 'DELETE'
+        })
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+            }
+          })
+      }
     })
 
   }
@@ -40,24 +43,30 @@ const MyCart = () => {
     <div>
       <SectionTitle heading="WANNA ADD MORE?" subtitle="My Cart"></SectionTitle>
 
+      <div  className="mx-[160px] uppercase text-xl font-bold py-3 flex justify-between">
+        <h1>Total orders: {cart.length}</h1>
+        <h1>total price: ${totalPrice}</h1>
+        <button className="bg-[#D1A054] text-white text-[15px] px-2 rounded">PAY</button>
+      </div>
+
       <div className="flex justify-center text-left">
         <div className="">
           <table className="md:w-[600px]">
             {/* head */}
-              <tr className="bg-[#D1A054]  text-white h-[40px] rounded-xl my-10">
-                <th>
-                  <label>
-                    <p>#</p>
-                  </label>
-                </th>
-                <th>ITEM IMAGE</th>
-                <th>ITEM NAME</th>
-                <th>PRICE</th>
-                <th>ACTION</th>
-              </tr>
+            <tr className="bg-[#D1A054]  text-white h-[40px] !rounded-xl my-10">
+              <th>
+                <label>
+                  <p>#</p>
+                </label>
+              </th>
+              <th>ITEM IMAGE</th>
+              <th>ITEM NAME</th>
+              <th>PRICE</th>
+              <th>ACTION</th>
+            </tr>
 
-              {cart.map((c, index) => (
-                <>
+            {cart.map((c, index) => (
+              <>
                 <tr key={c._id} className="my-10">
                   <th>
                     <label>
@@ -77,9 +86,9 @@ const MyCart = () => {
                     </div>
                   </td>
                   <td>
-                  <div>
-                        <div className="font-bold">{c.name}</div>
-                      </div>
+                    <div>
+                      <div className="font-bold">{c.name}</div>
+                    </div>
                   </td>
                   <td>
                     <span className="badge badge-ghost badge-sm">
@@ -87,12 +96,12 @@ const MyCart = () => {
                     </span>
                   </td>
                   <th>
-                    <button onClick={()=>handelDelete(c._id)} className="btn btn-ghost text-2xltext-white"><RiDeleteBin5Line></RiDeleteBin5Line></button>
+                    <button onClick={() => handelDelete(c._id)} className="btn btn-ghost text-2xltext-white"><RiDeleteBin5Line></RiDeleteBin5Line></button>
                   </th>
                 </tr>
-                  {/* <hr className="text-2xl w-10 text-red-700" /> */}
-                </>
-              ))}
+                {/* <hr className="text-2xl w-10 text-red-700" /> */}
+              </>
+            ))}
           </table>
         </div>
       </div>
--- src/pages/Dashboard/Reservation/Reservation.jsx
+++ src/pages/Dashboard/Reservation/Reservation.jsx
@@ -1,7 +1,92 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { MdOutlineTableRestaurant } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
+import Swal from "sweetalert2";
+
 const Reservation = () => {
+    const { user } = useContext(AuthContext);
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = (data) => {
+        console.log(data, user.email);
+
+        fetch('http://localhost:5000/reserve', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.insertedId){
+                    reset();
+                     Swal.fire({
+                         position: 'top-end',
+                         icon: 'success',
+                         title: 'Book A Table Success.',
+                         showConfirmButton: false,
+                         timer: 1500
+                       })
+                 }
+            })
+    }
+
     return (
         <div>
-            <h1>Reservation</h1>
+            <SectionTitle heading="BOOK A TABLE" subtitle="Reservation" ></SectionTitle>
+            <div className="flex justify-center m-8">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="grid grid-cols-3 border-3 gap-10">
+                        <div>
+                            <p>Date*</p>
+                            <input type="date"
+                                {...register("date", { required: true })}
+                                className="border-2 rounded w-full p-2" />
+                        </div>
+                        <div>
+                            <p>Time*</p>
+                            <input type="time"
+                                {...register("time", { required: true })}
+                                className="border-2 rounded w-full p-2" />
+                        </div>
+                        <div>
+                            <p>Gust*</p>
+                            <select
+                                {...register("person", { required: true })}
+                                className="select select-bordered w-full max-w-xs">
+                                <option disabled selected >Select</option>
+                                <option>1 Person</option>
+                                <option>2 Person</option>
+                                <option>3 Person</option>
+                                <option>4 Person</option>
+                                <option>5 Person</option>
+                                <option>6 Person</option>
+                            </select>
+                        </div>
+                        <div>
+                            <p>Name*</p>
+                            <input type="text" placeholder="Your Name"
+                                {...register("name", { required: true })}
+                                className="border-2 rounded w-full p-2" />
+                        </div>
+                        <div>
+                            <p>Phone*</p>
+                            <input type="number" name="" id="" placeholder="Phone Number"
+                                {...register("number", { required: true })}
+                                className="border-2 rounded w-full p-2" />
+                        </div>
+                        <div>
+                            <p>Email*</p>
+                            <input type="email" name="email" id="email" value={user?.email} readOnly
+                                {...register("email", { required: true })}
+                                className="border-2 rounded w-full p-2" />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn flex mx-auto bg-[#D1A054] border-0 hover:bg-[#D99904] my-5">Book A Table <MdOutlineTableRestaurant className="text-[28px] pl-1"></MdOutlineTableRestaurant></button>
+                </form>
+            </div>
         </div>
     );
 };
